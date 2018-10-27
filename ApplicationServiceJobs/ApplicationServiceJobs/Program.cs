6f5ddb88f0988f29b60b48d4843fa130eb108a01
Oscar Rueda <alonso@leapfactor.com>
2018-10-27 11:16:39 -0500

    added Application Service Jobs
    Signed-off-by: Oscar Rueda <alonso@leapfactor.com>

---
+++ ApplicationServiceJobs/ApplicationServiceJobs/Program.cs
@@ -0,0 +1,84 @@
+﻿using Quartz;
+using Quartz.Impl;
+using Quartz.Logging;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+using ApplicationServiceJobs.Jobs;
+
+namespace ApplicationServiceJobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
+
+            RunProgramRunExample().GetAwaiter().GetResult();
+
+            Console.WriteLine("Press any key to close the application");
+            Console.ReadKey();
+
+        }
+
+        private static void ConfigureJobs(IScheduler scheduler)
+        {
+            SetJob<HelloJob>(scheduler, "JobFlowRunner", "Group1", 1);
+        }
+
+        private static async Task RunProgramRunExample()
+        {
+            try
+            {
+                // Grab the Scheduler instance from the Factory
+                NameValueCollection props = new NameValueCollection
+                {
+                    { "quartz.serializer.type", "binary" }
+                };
+                StdSchedulerFactory factory = new StdSchedulerFactory(props);
+                IScheduler scheduler = await factory.GetScheduler();
+
+                // and start it off
+                await scheduler.Start();
+
+                ConfigureJobs(scheduler);
+                
+            }
+            catch (SchedulerException se)
+            {
+                Console.WriteLine(se);
+            }
+        }
+
+        private static void SetJob<T>(IScheduler scheduler, string namePrefix, string groupName, int withIntervalInMinutes) where T : IJob
+        {
+            if (withIntervalInMinutes == 0)
+                return;
+
+            // define the job and tie it to our HelloJob class
+            var runnerJob = JobBuilder.Create<T>()
+                .WithIdentity($"{namePrefix}_Job", groupName)
+                .Build();
+
+            // Trigger the job to run now, and then repeat every 10 seconds
+            var runnerTrigger = TriggerBuilder.Create()
+                .WithIdentity($"{namePrefix}_Schedule", groupName)
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInMinutes(withIntervalInMinutes)
+                    .RepeatForever()).Build();
+
+            scheduler.ScheduleJob(runnerJob, runnerTrigger);
+            var nextFireTime = runnerTrigger.GetNextFireTimeUtc();
+
+            if (nextFireTime != null)
+                Console.WriteLine($"Next {namePrefix} RunnerJob Fire Time: {nextFireTime.Value}");
+        }
+
+    }
+
+    
+}
+
+
+
